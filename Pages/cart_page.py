5407cd058f3a5d85c61c39e49639fdad1f610df5
Irina <dojdlivaia@bk.ru>
2019-08-01 03:21:05 +0400

    add cheks for basket Page

---
+++ Pages/cart_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class CartPage(BasePage):
+    def should_be_basket_clear(self):
+       assert self.is_not_element_present(*BasketPageLocators.BASKET_CONTENT),"Cart is not clear"
+
+    def clear_basket_message(self):
+        assert "Your basket is empty" in self.browser.find_element(*BasketPageLocators.BASKET_TEXT).text, "Text not 'is empty'"      
+
+
+#    def should_be_login_form(self):
+#        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+#    def should_be_register_form(self):
+#        assert self.is_element_present(*LoginPageLocators.REGISTOR_FORM) , "Register form is not presented"
